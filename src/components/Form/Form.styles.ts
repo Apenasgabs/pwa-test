48562caf939813f12660b43c96f88d7c2b0b3302
Gabriel Rodrigues <gabers357@gmail.com>
2023-10-02 12:34:20 -0300

    lint fixes

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,15 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledVideoContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-	padding-top: 20px;
-	video {
-		width: 90vw;
-		border-radius: 4px;
-		/* height: 50vh; */
-	}
+  padding-top: 20px;
+  video {
+    width: 90vw;
+    border-radius: 4px;
+    /* height: 50vh; */
+  }
 `;
